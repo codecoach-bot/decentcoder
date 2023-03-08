77d52b539c8104d81028ddfb889b71460f3107de
kiranbhoi <kiran.bhoi@parallel-minds.com>
2023-03-08 22:09:29 +0530

    new update

--- typescript/app.js
+++ typescript/app.js
@@ -19,21 +19,111 @@ obj.detail();
 
 const objCopy = {name: "hello"};
 */
-class department {
-    constructor(n) {
-        this.employee = [];
+/*
+class department{
+    empName: string;
+    employee: string[] = [];
+
+    constructor(n: string){
         this.empName = n;
     }
-    departmentDetails() {
-        console.log("Employee Details are:" + this.empName);
-        console.log("Employee Details are:" + this.employee.length);
+    departmentDetails(){
+        console.log("Employee Details are:"+ this.employee.length);
+        console.log("Employee Details are:"+ this.empName);
+        
     }
-    addEmployee(employee) {
+    addEmployee(employee: string){
         this.employee.push(employee);
     }
+
 }
-const Emp = new department("Sunil");
-Emp.addEmployee("Max");
-Emp.addEmployee("san");
-Emp.addEmployee("axx");
-Emp.departmentDetails();
+
+class Employee extends department{
+    get lastreport(){
+        return this.report;
+    }
+    set lastreport(report){
+        this.report = report;
+    }
+
+    constructor(public id: number, public name: string, private report: string) {
+        super("Akshay");
+        this.report = report;
+        this.id = id;
+        this.name = name;
+    }
+
+    empdetails(){
+        console.log("Employee details:" +
+        `Empid: ${this.id}
+        EmpName: ${this.name}`);
+    }
+
+}
+const obj = new Employee(1, "yash");
+
+obj.addEmployee("sona");
+obj.empdetails();
+
+
+
+// Emp.addEmployee("Max");
+// Emp.addEmployee("san");
+// Emp.addEmployee("axx");
+
+// Emp.departmentDetails();
+
+*/
+// interface 
+// interface can be use as the function okay.
+/*
+interface addMe{
+    (a:number, b: number, c: string): number;
+
+}
+
+
+interface Person{
+name: string;
+age: number;
+height: number;
+info(phrase: string): void;
+
+}
+
+class Greed implements Person{
+name: string;
+age: number;
+height: number;
+
+constructor(n: string, a: number, h: number){
+    this.name = n;
+    this.age = a;
+    this.height =h;
+}
+
+info(phrase: string){
+    console.log(phrase+ " "+ this.name);
+}
+
+}
+
+let user1: Person;
+user1 = {
+    name :"manisha",
+    age: 23,
+    height: 5.1,
+    info(phrase: string){
+        console.log(phrase+ " "+ this.name);
+    }
+};
+
+// user1.info("today manisha is very serious mode...");
+// console.log(user1);
+
+user1 = new Greed('Manu', 22, 4);
+console.log(user1);
+*/
+let full = true;
+var nameMy = "kiran";
+const Sumof = undefined;
--- typescript/app.ts
+++ typescript/app.ts
@@ -18,6 +18,7 @@ obj.detail();
 
 const objCopy = {name: "hello"};
 */
+/*
 class department{
     empName: string;
     employee: string[] = [];
@@ -35,11 +36,96 @@ class department{
     }
 
 }
-const Emp = new department("Sunil");
-Emp.addEmployee("Max");
-Emp.addEmployee("san");
-Emp.addEmployee("axx");
 
-Emp.departmentDetails();
+class Employee extends department{
+    get lastreport(){
+        return this.report;
+    }
+    set lastreport(report){
+        this.report = report;
+    }
+
+    constructor(public id: number, public name: string, private report: string) {
+        super("Akshay");
+        this.report = report;
+        this.id = id;
+        this.name = name;
+    }
+
+    empdetails(){
+        console.log("Employee details:" + 
+        `Empid: ${this.id}
+        EmpName: ${this.name}`);
+    }
+
+}
+const obj = new Employee(1, "yash");
+
+obj.addEmployee("sona");
+obj.empdetails();
+
+
+
+// Emp.addEmployee("Max");
+// Emp.addEmployee("san");
+// Emp.addEmployee("axx");
+
+// Emp.departmentDetails();
+
+*/
+
+// interface 
+
+// interface can be use as the function okay.
+/*
+interface addMe{
+    (a:number, b: number, c: string): number;
+
+}
+
+
+interface Person{
+name: string;
+age: number;
+height: number;
+info(phrase: string): void;
+
+}
+
+class Greed implements Person{
+name: string;
+age: number;
+height: number;
+
+constructor(n: string, a: number, h: number){
+    this.name = n;
+    this.age = a;
+    this.height =h;
+}
+
+info(phrase: string){
+    console.log(phrase+ " "+ this.name);
+}
+
+}
+
+let user1: Person;
+user1 = {
+    name :"manisha",
+    age: 23,
+    height: 5.1,
+    info(phrase: string){
+        console.log(phrase+ " "+ this.name);
+    }
+};
+
+// user1.info("today manisha is very serious mode...");
+// console.log(user1);
+
+user1 = new Greed('Manu', 22, 4);
+console.log(user1);
+*/
+
+
 
 
